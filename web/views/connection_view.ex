045ff8f25ea04ec0856326431276f4387a9b2aa4
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2016-04-30 20:40:49 +0200

    Refactored full_name. DRY. closes #23

--- web/views/connection_view.ex
+++ web/views/connection_view.ex
@@ -1,13 +1,4 @@
 defmodule Vutuv.ConnectionView do
   use Vutuv.Web, :view
-  alias Vutuv.User
-
-  def full_name(%User{first_name: first_name,
-                      last_name: last_name,
-                      honorific_prefix: honorific_prefix,
-                      honorific_suffix: honorific_suffix}) do
-    [honorific_prefix, first_name, last_name, honorific_suffix]
-    |> Enum.reject(&(&1 == ""))
-    |> Enum.join(" ")
-  end
+  import Vutuv.UserHelpers 
 end
--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,15 +1,6 @@
 defmodule Vutuv.UserView do
   use Vutuv.Web, :view
-  alias Vutuv.User
-
-  def full_name(%User{first_name: first_name,
-                      last_name: last_name,
-                      honorific_prefix: honorific_prefix,
-                      honorific_suffix: honorific_suffix}) do
-    [honorific_prefix, first_name, last_name, honorific_suffix]
-    |> Enum.reject(&(&1 == ""))
-    |> Enum.join(" ")
-  end
+  import Vutuv.UserHelpers
 
   def avatar_url(user) do
     Vutuv.Avatar.url({user.avatar, user}, :original)
