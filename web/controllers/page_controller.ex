24f29cb458168afcf2dde51bce882d1d383956fd
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2016-05-01 09:26:41 +0200

    Displays the number of users on the index page.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule Vutuv.PageController do
   use Vutuv.Web, :controller
+  alias Vutuv.User
 
   def index(conn, _params) do
-    render conn, "index.html"
+    user_count = Repo.one(from u in User, select: count("*"))
+    render conn, "index.html", user_count: user_count
   end
 end
