0a5b4f2f4a6a3bf319d75c016c59add4ef14b60c
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2016-05-01 12:33:20 +0200

    Fixes a test.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Vutuv.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to vutuv!"
   end
 end
