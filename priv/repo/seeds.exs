5eb245ae0ebf7cf64ab39c81384affff9ffaf538
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2016-05-03 19:43:07 +0200

    Adds gender to the seeds.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,10 +7,10 @@
 #
 #     Vutuv.Repo.insert!(%Vutuv.SomeModel{})
 
-sw = Vutuv.Repo.insert! (%Vutuv.User{first_name: "Stefan", last_name: "Wintermeyer"})
+sw = Vutuv.Repo.insert! (%Vutuv.User{first_name: "Stefan", last_name: "Wintermeyer", gender: "male"})
 Vutuv.Repo.insert! (%Vutuv.Email{value: "stefan.wintermeyer@amooma.de", user_id: sw.id})
 Vutuv.Repo.insert! (%Vutuv.Email{value: "sw@amooma.de", user_id: sw.id})
 
-Vutuv.Repo.insert! (%Vutuv.User{first_name: "Oliver", last_name: "Andrich"})
-Vutuv.Repo.insert! (%Vutuv.User{first_name: "Lennex", last_name: "Zinyando"})
-Vutuv.Repo.insert! (%Vutuv.User{first_name: "Kasper", last_name: "Tidemann"})
+Vutuv.Repo.insert! (%Vutuv.User{first_name: "Oliver", last_name: "Andrich", gender: "male"})
+Vutuv.Repo.insert! (%Vutuv.User{first_name: "Lennex", last_name: "Zinyando", gender: "male"})
+Vutuv.Repo.insert! (%Vutuv.User{first_name: "Kasper", last_name: "Tidemann", gender: "male"})
