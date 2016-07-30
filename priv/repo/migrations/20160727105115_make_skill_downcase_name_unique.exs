e758d57471a649f54ec08782850005c69d154354
Chris <chrstnfreeze@gmail.com>
2016-07-30 15:07:10 -0500

    MySql is up and running on my local development environment. Migration fixes have been tested to work. Closes #68.

--- priv/repo/migrations/20160727105115_make_skill_downcase_name_unique.exs
+++ priv/repo/migrations/20160727105115_make_skill_downcase_name_unique.exs
@@ -2,7 +2,7 @@ defmodule Vutuv.Repo.Migrations.MakeSkillDowncaseNameUnique do
   use Ecto.Migration
 
   def change do
-  	drop_if_exists index(:skills, [:name])
+  	#drop index(:skills, [:name])
   	create index(:skills, [:downcase_name], unique: true)
   end
 end
