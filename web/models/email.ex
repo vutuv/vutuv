ea4f8458fbc204f39b3455aaf09efdc6c1dfc202
Chris <chrstnfreeze@gmail.com>
2016-08-01 10:44:01 -0500

    Fixed deprecated use of :empty. All models now use an empty struct. Closes #74

--- web/models/email.ex
+++ web/models/email.ex
@@ -18,7 +18,7 @@ defmodule Vutuv.Email do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
     model
     |> cast(params, @required_fields++@optional_fields)
     |> downcase_value
--- web/models/endorsement.ex
+++ web/models/endorsement.ex
@@ -18,7 +18,7 @@ defmodule Vutuv.Endorsement do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
     model
     |> cast(params, @required_fields++@optional_fields)
     |> unique_constraint(:user_id_user_skill_id)
