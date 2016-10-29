280ae5a419028e7f1947e17d51925c5909b11414
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2016-10-29 17:17:11 +0200

    users#create redirects to => (to: page_path(conn, :new_registration))
    This is a better UI workflow for the user of the system.

--- web/router.ex
+++ web/router.ex
@@ -29,6 +29,7 @@ defmodule Vutuv.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/new_registration", PageController, :new_registration
 
     # TODO: delete the following route entries
     resources "/memberships", MembershipController
