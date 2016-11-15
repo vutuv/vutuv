386fd966c2ca152c654f8a3fe428039a8d814cd7
Chris <chrstnfreeze@gmail.com>
2016-11-15 10:20:12 -0600

    4th Test for config entry solution

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -4,13 +4,10 @@ defmodule Vutuv.LayoutView do
 
   defp embed_css(conn) do
     filepath = "/home/vutuv/Github/vutuv/priv/static/css/app.css"
-    alt_filepath = Mix.Project.app_path<>"/priv/static"<>static_path(conn, "/css/app.css")
     debug = ~s[<!--
       file debug\n
       filepath #{inspect filepath}\n
-      filepath2 #{inspect alt_filepath}\n
       filepath exists?: #{inspect(File.exists?(filepath))}\n
-      filepath2 exists?: #{inspect(File.exists?(alt_filepath))}\n
       -->\n
     ]
     case File.read(filepath) do
