1b8b608491ad0ef1b93425b30e794d0bc34417e7
Chris <chrstnfreeze@gmail.com>
2016-11-15 10:06:02 -0600

    Another possible filepath fix

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -4,13 +4,16 @@ defmodule Vutuv.LayoutView do
 
   defp embed_css(conn) do
     filepath = Application.app_dir(:vutuv, "/priv/static")<>static_path(conn, "/css/app.css")
+    alt_filepath = Mix.Project.app_path<>"/priv/static"<>static_path(conn, "/css/app.css")
     debug = ~s[<!--
       file debug\n
       filepath #{inspect filepath}\n
+      filepath2 #{inspect alt_filepath}\n
       filepath exists?: #{inspect(File.exists?(filepath))}\n
+      filepath2 exists?: #{inspect(File.exists?(alt_filepath))}\n
       -->\n
     ]
-    case File.read(filepath) do
+    case File.read(alt_filepath) do
       {:ok, data} -> "#{debug}<style>\n#{data}\n</style>"
       {:error, _} -> "#{debug}<link rel=\"stylesheet\" href=\"#{static_path(conn, "/css/app.css")}\">"
     end
