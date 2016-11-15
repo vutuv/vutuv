6acab26c487ffee8eb5cb623fa2024775569d51b
Chris <chrstnfreeze@gmail.com>
2016-11-15 08:06:03 -0600

    Addded debug code for prod file errors

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -4,9 +4,18 @@ defmodule Vutuv.LayoutView do
 
   defp embed_css(conn) do
     filepath = File.cwd!<>"/priv/static"<>static_path(conn, "/css/app.css")
+    debug = ~s[<!--
+      file debug\n
+      just static_path: #{File.exists?(static_path(conn, "/css/app.css"))}\n
+      static_path with /priv/static appended: #{File.exists?("/priv/static"<>static_path(conn, "/css/app.css"))}\n
+      static_path priv/static and cwd: #{File.exists?(File.cwd!<>"/priv/static"<>static_path(conn, "/css/app.css"))}\n
+      static_path with cwd: #{File.exists?(File.cwd!<>"/priv/static"<>static_path(conn, "/css/app.css"))}\n
+      cwd: #{File.exists?(File.cwd!)}\n
+      -->\n
+    ]
     case File.read(filepath) do
-      {:ok, data} -> "<style>\n#{data}\n</style>"
-      {:error, _} -> "<link rel=\"stylesheet\" href=\"#{static_path(conn, "/css/app.css")}\">"
+      {:ok, data} -> "#{debug}<style>\n#{data}\n</style>"
+      {:error, _} -> "#{debug}<link rel=\"stylesheet\" href=\"#{static_path(conn, "/css/app.css")}\">"
     end
     |> raw
   end
