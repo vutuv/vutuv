1a2cfa7c95a868b2bd7c1fe22c48c75b33292027
Chris <chrstnfreeze@gmail.com>
2016-11-15 09:50:59 -0600

    Possible cross platform filepath fix

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Vutuv.Mixfile do
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
+     build_embedded: Mix.env == :dev,
      start_permanent: Mix.env == :prod,
      aliases: aliases,
      deps: deps]
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -3,14 +3,11 @@ defmodule Vutuv.LayoutView do
   import Vutuv.UserHelpers
 
   defp embed_css(conn) do
-    filepath = File.cwd!<>static_path(conn, "/css/app.css")
+    filepath = Application.app_dir(:vutuv, "/priv/static")<>static_path(conn, "/css/app.css")
     debug = ~s[<!--
       file debug\n
-      just static_path: #{inspect(static_path(conn, "/css/app.css"))}\n
-      static_path with /priv/static appended: #{inspect("/priv/static"<>static_path(conn, "/css/app.css"))}\n
-      static_path priv/static and cwd: #{inspect(File.cwd!<>"/priv/static"<>static_path(conn, "/css/app.css"))}\n
-      static_path with cwd: #{inspect(File.cwd!<>static_path(conn, "/css/app.css"))}\n
-      cwd: #{inspect(File.cwd!)}\n
+      filepath #{inspect filepath}\n
+      filepath exists?: #{inspect(File.exists?(filepath))}\n
       -->\n
     ]
     case File.read(filepath) do
