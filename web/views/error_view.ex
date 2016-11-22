a0e573cd642be9f59618362a64967ee8ada26245
Chris <chrstnfreeze@gmail.com>
2016-11-22 09:27:20 -0600

    Added temporary error routes

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -2,15 +2,21 @@ defmodule Vutuv.ErrorView do
   use Vutuv.Web, :view
 
   def render("404.html", _assigns) do
-    "Page not found"
+    "</header> <h1 style=\"text-align:center;\">Page not found</h1>"
+    |> Vutuv.Gettext.gettext
+    |> Phoenix.HTML.raw
   end
 
   def render("403.html", _assigns) do
-    "403 Forbidden"
+    "</header> <h1 style=\"text-align:center;\">You are not allowed to view this page.</h1>"
+    |> Vutuv.Gettext.gettext
+    |> Phoenix.HTML.raw
   end
 
   def render("500.html", _assigns) do
-    "Server internal error"
+    "</header> <h1 style=\"text-align:center;\">Pardon us! Something went wrong. If you think this is a bug, please <a href = \"https://github.com/vutuv/vutuv/issues/new\"submit a bug report.</a></h1>"
+    |> Vutuv.Gettext.gettext
+    |> Phoenix.HTML.raw
   end
 
   def render("error.json", _assigns) do
@@ -20,6 +26,8 @@ defmodule Vutuv.ErrorView do
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
-    render "500.html", assigns
+    "</header> <h1 style=\"text-align:center;\">Pardon us! Something went wrong. If you think this is a bug, please <a href = \"https://github.com/vutuv/vutuv/issues/new\"submit a bug report.</a></h1>"
+    Vutuv.Gettext.gettext
+    |> Phoenix.HTML.raw
   end
 end
