4f8a47ca1d5a7c9d1028ba64d6f749ed2c59355f
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2016-11-24 15:00:13 +0100

    Bump version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Vutuv.Mixfile do
 
   def project do
     [app: :vutuv,
-     version: "1.0.45",
+     version: "1.0.46",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
