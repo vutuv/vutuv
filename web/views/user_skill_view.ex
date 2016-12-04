0d9dffdd927a4388be95ad615c367c3e8dd08048
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2016-12-04 13:18:26 +0100

    Refactor rendering of endorsement counts.

--- web/views/user_skill_view.ex
+++ web/views/user_skill_view.ex
@@ -2,4 +2,16 @@ defmodule Vutuv.UserSkillView do
   use Vutuv.Web, :view
   import Vutuv.UserHelpers
   alias Vutuv.Skill
+
+  def render_upvotes(number) do
+    case {number} do
+      {1} ->
+        ['1 ', Vutuv.Gettext.gettext("upvote")]
+      {number} when is_integer(number) ->
+        [Integer.to_string(number), ' ', Vutuv.Gettext.gettext("upvotes")]
+      _ ->
+        []
+    end
+  end
+
 end
