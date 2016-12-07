973b45060d92d32d2405422d882bc904b61b59e5
Chris <chrstnfreeze@gmail.com>
2016-12-06 21:39:22 -0600

    Unlocked and upgraded /skills route
    Skills allows for the viewing of skill#show pages. These pages
    feature lists of users that have the skill that are either
    top in endorsements, or connected to the current user. Also
    includes a button to add the skill if the current_user
    does not have the skill. Closes #478. Closes #479.

--- web/controllers/endorsement_controller.ex
+++ web/controllers/endorsement_controller.ex
@@ -1,10 +1,11 @@
 defmodule Vutuv.EndorsementController do
   use Vutuv.Web, :controller
+  plug :resolve_slug
 
   alias Vutuv.Endorsement
 
-  def create(conn, %{"id" => id}) do
-    changeset = Endorsement.changeset(%Endorsement{}, %{user_skill_id: id, user_id: conn.assigns[:current_user].id})
+  def create(conn, params) do
+    changeset = Endorsement.changeset(%Endorsement{}, %{user_skill_id: conn.assigns[:user_skill_id], user_id: conn.assigns[:current_user_id]})
     case Repo.insert(changeset) do
       {:ok, _user_skill} ->
         conn
@@ -17,8 +18,8 @@ defmodule Vutuv.EndorsementController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    Repo.one!(from e in Vutuv.Endorsement, where: e.user_skill_id==^id and e.user_id==^conn.assigns[:current_user].id )
+  def delete(conn, params) do
+    Repo.one!(from e in Vutuv.Endorsement, where: e.user_skill_id==^conn.assigns[:user_skill_id] and e.user_id==^conn.assigns[:current_user_id] )
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
     |>Repo.delete!
@@ -41,4 +42,18 @@ defmodule Vutuv.EndorsementController do
       user_path(conn, :show, conn.assigns[:user])
     end
   end
+
+  defp resolve_slug(%{params: %{"id" => slug}} = conn, _) do
+    Repo.one(from w in assoc(conn.assigns[:user], :user_skills), join: s in assoc(w, :skill), where: s.slug == ^slug, select: w.id)
+    |>case do
+      nil -> 
+        conn
+        |> put_status(404)
+        |> render(Vutuv.ErrorView, "404.html")
+      id -> assign(conn, :user_skill_id, id)
+    end
+    
+  end
+
+  defp resolve_slug(conn, _), do: conn
 end
