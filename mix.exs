cac247273547f81217443d4ed68c040e5bf96f4e
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2017-01-19 17:49:47 +0100

    s/elixir: "~> 1.0"/elixir: "~> 1.4.0"/

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vutuv.Mixfile do
   def project do
     [app: :vutuv,
      version: "1.2.43",
-     elixir: "~> 1.0",
+     elixir: "~> 1.4.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
