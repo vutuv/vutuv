ab49a8cd42b3cc7bd9f944de85d6df752c68f664
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2017-03-03 16:40:23 +0100

    Uses number_to_currency to display price.

--- web/views/admin/recruiter_package_view.ex
+++ web/views/admin/recruiter_package_view.ex
@@ -1,6 +1,7 @@
 defmodule Vutuv.Admin.RecruiterPackageView do
   use Vutuv.Web, :view
   import Vutuv.UserHelpers
+  import Number.Currency
 
   defp get_currency_symbol("dollar"), do: "$"
   defp get_currency_symbol("euro"), do: "€"
