5277d7e90b29141d2304cbd49a0b025375cf9418
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2017-03-07 13:25:16 +0100

    Add "http://" before uncomplete urls.

--- web/views/url_view.ex
+++ web/views/url_view.ex
@@ -1,4 +1,12 @@
 defmodule Vutuv.UrlView do
   use Vutuv.Web, :view
   import Vutuv.UserHelpers
+
+  def linkable_url(string) do
+    if Enum.count(String.split(string, "://")) > 1 do
+      string
+    else
+      "http://#{string}"
+    end
+  end
 end
