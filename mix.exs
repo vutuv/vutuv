6de6ed9679abf5d37c829a6096fda228b339da4a
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2019-03-03 08:45:04 +0100

    Bump version. s/rand_bytes/strong_rand_bytes/g in production. #569

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Vutuv.Mixfile do
 
   def project do
     [app: :vutuv,
-     version: "1.6.17",
+     version: "1.6.18",
      elixir: "~> 1.5.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
