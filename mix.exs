8435f28f0c23009b0216df9079aad58b07d245cb
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2019-03-03 07:47:58 +0100

    Bump version to test deploy a fix for #569

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Vutuv.Mixfile do
 
   def project do
     [app: :vutuv,
-     version: "1.6.10",
+     version: "1.6.11",
      elixir: "~> 1.5.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
